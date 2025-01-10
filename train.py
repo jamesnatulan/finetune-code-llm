f3c7ae70b9455814f394699f1a41ecf4223e2ee6
James Natulan <jamesnatulan@gmail.com>
2025-01-10 13:09:40 +0800

    Save to gguf

--- train.py
+++ train.py
@@ -8,6 +8,8 @@ from transformers import TrainingArguments
 import os
 from tqdm import tqdm
 from functools import partial
+import ollama
+import re
 
 from fim import ConstantLengthDataset
 
@@ -20,7 +22,7 @@ PARAM_SIZE = "0.5"  # Model parameter size
 BASE_OUTPUT_DIR = "runs"
 
 # Training parameters
-MAX_STEPS = 2000  # max_steps
+MAX_STEPS = 1  # max_steps
 BATCH_SIZE = 16  # batch_size
 GR_ACC_STEPS = 1  # gradient_accumulation_steps
 LR = 5e-4  # learning_rate
@@ -40,6 +42,9 @@ SEED = 42
 # FIM trasformations arguments
 FIM_RATE = 0.5  # fim_rate
 FIM_SPM_RATE = 0.5  # fim_spm_rate
+
+# GGUF Options
+GGUF_QUANT_METHOD = "q4_k_m"
 
 
 def get_device():
@@ -65,6 +70,44 @@ def chars_token_ratio(dataset, tokenizer, data_column, nb_examples=400):
         total_tokens += len(tokenizer(example[data_column]).tokens())
 
     return total_characters / total_tokens
+
+def parse_modelfile(modelfile: str):
+    """
+    Parse the modelfile from Ollama
+    """
+    
+    keywords = ["FROM", "PARAMETER", "TEMPLATE", "SYSTEM", "LICENSE", "ADAPTER", "MESSAGE"]
+
+    # Check if modelfile is path and read the file
+    if os.path.isfile(modelfile):
+        with open(modelfile, "r") as f:
+            modelfile_text = f.read()
+    else: # Assume modelfile is the text itself
+        modelfile_text = modelfile
+
+    # Remove commented lines
+    modelfile_text = re.sub(r'#.*', '', modelfile_text)
+
+    # Split the text by keywords, up until next keyword
+    pattern = r'\b(?:' + '|'.join(map(re.escape, keywords)) + r')\b'
+    matches = list(re.finditer(pattern, modelfile_text))
+
+    # Loop through matches and pair keywords with the text after them
+    pairs = []
+    for i, match in enumerate(matches):
+        keyword = match.group(0)
+        start, end = match.span()
+        
+        # Determine the text after this keyword
+        if i + 1 < len(matches):
+            next_start = matches[i + 1].start()  # Start of the next keyword
+            after_text = modelfile_text[end:next_start].strip()
+        else:
+            after_text = modelfile_text[end:].strip()  # Remaining text after the last keyword
+        
+        pairs.append((keyword, after_text))
+
+    return pairs
 
 
 def main():
@@ -165,6 +208,7 @@ def main():
         dataloader_drop_last=True,
         eval_strategy="steps",
         save_strategy="steps",
+        report_to="tensorboard",
         max_steps=MAX_STEPS,
         eval_steps=EVAL_FREQ,
         save_steps=SAVE_FREQ,
@@ -193,10 +237,51 @@ def main():
     trainer.train()
 
     # Export the model and save to ollama
-    model.save_pretrained(output_dir)
-    tokenizer.save_pretrained(output_dir)
+    final_output_dir = os.path.join(output_dir, "final")
+    os.makedirs(final_output_dir, exist_ok=True)
 
-    model.save_pretrained_gguf(output_dir, tokenizer, quantization_method="q4_k_m")
+    model.save_pretrained(final_output_dir)
+    tokenizer.save_pretrained(final_output_dir)
+
+    # Save gguf model
+    model.save_pretrained_gguf(final_output_dir, tokenizer, quantization_method="q4_k_m")
+
+    # Rename the output files
+    os.rename(
+        os.path.join(final_output_dir, f"unsloth.{GGUF_QUANT_METHOD.upper()}.gguf"),
+        os.path.join(final_output_dir, f"model_{GGUF_QUANT_METHOD}.gguf"),
+    )
+    print(f"Renamed: unsloth.{GGUF_QUANT_METHOD.upper()}.gguf -> model_{GGUF_QUANT_METHOD}.gguf")
+
+    # Generate a Modelfile for ollama
+    from_path = os.path.abspath(os.path.join(final_output_dir, f"model_{GGUF_QUANT_METHOD}.gguf"))
+
+    # Get template and parameters from base model
+    # Check first if model is in Ollama list
+    ollama_list = ollama.list()
+
+    # Format the response in a way that we can use it
+    models_list = []
+    for model in ollama_list.models:
+        models_list.append(model.model)
+
+    model_arch = f"qwen2.5-coder:{PARAM_SIZE}b"
+    if model_arch not in models_list:
+        ollama.pull(model_arch)
+
+    modelfile_txt = ollama.show(model_arch).modelfile
+    modelfile_pairs = parse_modelfile(modelfile_txt)
+    modelfile_values = [f"FROM {from_path}"]
+    for keyword, text in modelfile_pairs:
+        if keyword not in ["FROM", "LICENSE", "ADAPTER", "MESSAGE"]:
+            modelfile_values.append(f"{keyword} {text}")
+
+    modelfile_txt = "\n".join(modelfile_values)
+
+    modelfile_path = os.path.join(final_output_dir, "Modelfile")
+    with open(modelfile_path, "w") as f:
+        f.write(modelfile_txt)
+    print(f"Modelfile created at: {modelfile_path}")
 
 
 if __name__ == "__main__":
